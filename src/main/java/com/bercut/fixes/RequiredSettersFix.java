1a5ea18943b6aad60032f05ceb98dcb29d57374a
alyonazakharova <alenazakharovaspb@yandex.ru>
2021-10-26 12:53:01 +0300

    removed unused import

--- src/main/java/com/bercut/fixes/RequiredSettersFix.java
+++ src/main/java/com/bercut/fixes/RequiredSettersFix.java
@@ -11,7 +11,6 @@ import com.intellij.psi.codeStyle.CodeStyleManager;
 import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
